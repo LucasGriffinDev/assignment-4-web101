c0356fb9a595f7db8250af92d6a3b5ec61d78f7b
Lucas Griffin <66416659+LucasGriffinDev@users.noreply.github.com>
2023-10-08 23:33:32 +1100

    rego form

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const sameAsDeliveryCheckbox = document.getElementById('sameAsDelivery');
     const deliveryAddress = document.getElementById('deliveryAddress');
     const billingAddress = document.getElementById('billingAddress');
+    const creditCardType = document.getElementById('creditCardType');
+    const creditCardNumber = document.getElementById('creditCardNumber');
 
+
+    // form validation
+    const registrationForm = document.getElementById('registrationForm');
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    const email = document.getElementById('email');
+    const address = document.getElementById('address');
+    const termsCheckbox = document.getElementById('terms');
+
+    
     sameAsDeliveryCheckbox.addEventListener('change', function() {
         if (this.checked) {
             if (deliveryAddress.value.trim() === '') {
@@ -15,34 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
             billingAddress.value = '';
         }
     });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const sameAsDeliveryCheckbox = document.getElementById('sameAsDelivery');
-    const deliveryAddress = document.getElementById('deliveryAddress');
-    const billingAddress = document.getElementById('billingAddress');
-    const creditCardType = document.getElementById('creditCardType');
-    const creditCardNumber = document.getElementById('creditCardNumber');
-
-    // Handle copying the delivery address to the billing address
-    sameAsDeliveryCheckbox.addEventListener('change', function() {
-        if (this.checked) {
-            if (deliveryAddress.value.trim() === '') {
-                alert('Please enter your delivery address first.');
-                this.checked = false;
-            } else {
-                billingAddress.value = deliveryAddress.value;
-            }
-        } else {
-            billingAddress.value = '';
-        }
-    });
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const creditCardType = document.getElementById('creditCardType');
-    const creditCardNumber = document.getElementById('creditCardNumber');
 
     creditCardType.addEventListener('change', function() {
         switch (this.value) {
@@ -55,6 +39,36 @@ document.addEventListener('DOMContentLoaded', function() {
                 creditCardNumber.setAttribute('maxlength', '15');
                 creditCardNumber.setAttribute('minlength', '15');
                 break;
+        }
+    });
+    registrationForm.addEventListener('submit', function(e) {
+        // Array to store all error messages
+        let errors = [];
+
+        // Check if required fields are empty
+        if (username.value.trim() === '') {
+            errors.push('Username is required.');
+        }
+        if (password.value.trim() === '') {
+            errors.push('Password is required.');
+        }
+        if (password.value.trim().length < 8) {
+            errors.push('Password must be at least 8 characters long.');
+        }
+        if (email.value.trim() === '') {
+            errors.push('Email is required.');
+        }
+        if (address.value.trim() === '') {
+            errors.push('Address is required.');
+        }
+        if (!termsCheckbox.checked) {
+            errors.push('You must agree to the terms and conditions.');
+        }
+
+        // If there are any errors, prevent form submission and show error messages
+        if (errors.length > 0) {
+            e.preventDefault(); // Prevents the form from submitting
+            alert(errors.join('\n')); // Shows all the errors in an alert (you can choose to display them differently if you like)
         }
     });
 });
