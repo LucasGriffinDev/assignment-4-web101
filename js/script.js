43df7d96f7ccfcbf1336853b76fa5465993fefe1
Lucas Griffin <66416659+LucasGriffinDev@users.noreply.github.com>
2023-10-08 14:43:01 +1100

    feature page

--- js/script.js
+++ js/script.js
@@ -17,25 +17,28 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-function handleOrderTypeChange() {
-    const deliveryAddressDiv = document.getElementById('deliveryAddress');
-    const radios = document.getElementsByName('orderType');
-    if (radios[0].checked) {
-        deliveryAddressDiv.style.display = 'block';
-    } else {
-        deliveryAddressDiv.style.display = 'none';
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const sameAsDeliveryCheckbox = document.getElementById('sameAsDelivery');
+    const deliveryAddress = document.getElementById('deliveryAddress');
+    const billingAddress = document.getElementById('billingAddress');
+    const creditCardType = document.getElementById('creditCardType');
+    const creditCardNumber = document.getElementById('creditCardNumber');
 
-function handlePaymentMethodChange() {
-    const creditCardInfoDiv = document.getElementById('creditCardInfo');
-    const radios = document.getElementsByName('paymentMethod');
-    if (radios[1].checked) {
-        creditCardInfoDiv.style.display = 'block';
-    } else {
-        creditCardInfoDiv.style.display = 'none';
-    }
-}
+    // Handle copying the delivery address to the billing address
+    sameAsDeliveryCheckbox.addEventListener('change', function() {
+        if (this.checked) {
+            if (deliveryAddress.value.trim() === '') {
+                alert('Please enter your delivery address first.');
+                this.checked = false;
+            } else {
+                billingAddress.value = deliveryAddress.value;
+            }
+        } else {
+            billingAddress.value = '';
+        }
+    });
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     const creditCardType = document.getElementById('creditCardType');
@@ -46,9 +49,11 @@ document.addEventListener('DOMContentLoaded', function() {
             case 'Visa':
             case 'Mastercard':
                 creditCardNumber.setAttribute('maxlength', '16');
+                creditCardNumber.setAttribute('minlength', '16');
                 break;
             case 'American Express':
                 creditCardNumber.setAttribute('maxlength', '15');
+                creditCardNumber.setAttribute('minlength', '15');
                 break;
         }
     });
